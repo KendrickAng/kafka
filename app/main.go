c81f8d6082c2016fa98b0f55de751d2d2bc7c5b3
Kendrick Ang <kendrick@coinhall.org>
2025-05-13 22:05:24 +0800

    feat: send correlation id

--- app/main.go
+++ app/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -14,16 +17,49 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	if err := handleConnection(conn); err != nil {
+		fmt.Println("Error handling connection: ", err.Error())
+		os.Exit(1)
+	}
+}
+
+func handleConnection(conn net.Conn) error {
+	defer conn.Close()
+
+	clientMsg, err := io.ReadAll(conn)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Received message: ", string(clientMsg))
+
+	n, err := conn.Write(buildClientResponse())
+	fmt.Println("Wrote bytes: ", n)
+
+	return err
+}
+
+func buildClientResponse() []byte {
+	var buf bytes.Buffer
+
+	// message_size
+	var messageSize [4]byte
+	binary.BigEndian.PutUint32(messageSize[:], uint32(0))
+	buf.Write(messageSize[:])
+
+	// correlation_id
+	var correlationId int32 = 7
+	binary.Write(&buf, binary.BigEndian, correlationId)
+
+	return buf.Bytes()
 }
